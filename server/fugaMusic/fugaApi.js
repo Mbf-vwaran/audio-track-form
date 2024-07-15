c697197d7e6e4c5d5fc79597508086289636d7e3
Mbf-vwaran <vigneshwaran@madebyfire.com>
2024-07-15 18:33:57 +0530

    feat: fugaMusic api integrated
    Co-Authored-By: Vigneshwaran M <vignesh510510@gmail.com>

--- server/fugaMusic/fugaApi.js
+++ server/fugaMusic/fugaApi.js
@@ -1,5 +1,45 @@
 import axios from 'axios';
 
-async function fugaFunction (){
-    
+async function fugaFunction(gsToken, fugaData) {
+
+    try {
+        const response = await axios.post(`https://next.fugamusic.com/api/v2`,
+            fugaData,
+            {
+                headers: {
+                    'Authorization': `Bearer ${req.headers["gstoken"]}`
+                }
+            }
+        );
+        return response.data;
+    }
+    catch (error) {
+        console.error('unable to authorize, having some issues: ', error.response ? error.response.data : error.message);
+        throw error
+
+    }
+}
+
+async function getFugaData(gsToken) {
+
+    try {
+        const response = await axios.get(`https://next.fugamusic.com/api/v2/products/`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${req.headers["gstoken"]}`
+                }
+            }
+        );
+        return response.data;
+    }
+    catch (error) {
+        console.error('unable to authorize, having some issues:', error.response ? error.response.data : error.message);
+        throw error
+
+    }
+}
+
+module.exports = {
+    fugaFunction,
+    getFugaData
 }
